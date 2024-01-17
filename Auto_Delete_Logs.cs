498710283b707044f7bccb72f48864904a2b86c6
Gold KingZ <48490385+oqyh@users.noreply.github.com>
2024-01-17 17:03:08 +0400

    1.0.1

--- Auto_Delete_Logs.cs
+++ Auto_Delete_Logs.cs
@@ -7,16 +7,16 @@ public class AutoDeleteLogsConfig : BasePluginConfig
 {
     [JsonPropertyName("CounterstrikeSharpMoreThanXdaysOld")] public int CounterstrikeSharpMoreThanXdaysOld { get; set; } = 0;
     [JsonPropertyName("BackupRoundMoreThanXdaysOld")] public int BackupRoundMoreThanXdaysOld { get; set; } = 0;
+    [JsonPropertyName("DemoMoreThanXdaysOld")] public int DemoMoreThanXdaysOld { get; set; } = 0;
 }
 
 public class AutoDeleteLogs : BasePlugin, IPluginConfig<AutoDeleteLogsConfig>
 {
     public override string ModuleName => "Auto Delete Logs";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.0.1";
     public override string ModuleAuthor => "Gold KingZ";
     public override string ModuleDescription => "Auto Delete Logs Files";
     public AutoDeleteLogsConfig Config { get; set; } = new AutoDeleteLogsConfig();
-    public bool deletefolder = false;
     public void OnConfigParsed(AutoDeleteLogsConfig config)
     {
         Config = config;
@@ -24,7 +24,11 @@ public class AutoDeleteLogs : BasePlugin, IPluginConfig<AutoDeleteLogsConfig>
     
     public override void Load(bool hotReload)
     {
-        if(Config.CounterstrikeSharpMoreThanXdaysOld > 0)
+        RegisterListener<Listeners.OnMapStart>(OnMapStart);
+    }
+    private void OnMapStart(string Map)
+	{
+		if(Config.CounterstrikeSharpMoreThanXdaysOld > 0)
         {
             DeleteCounterstrikeSharp();
         }
@@ -33,52 +37,123 @@ public class AutoDeleteLogs : BasePlugin, IPluginConfig<AutoDeleteLogsConfig>
         {
             DeleteBackUP();
         }
-    }
+
+        if(Config.DemoMoreThanXdaysOld > 0)
+        {
+            DeleteDemos();
+        }
+	}
+
     private void DeleteCounterstrikeSharp()
     {
-        string folderPath = Path.Combine(ModuleDirectory, "../../plugins");
-        for (int i = 0; i < 1; i++)
+        try
         {
-            folderPath = Path.Combine(folderPath, "..");
-        }
-        string LOGS = Path.Combine(folderPath, "logs");
-
-        if (Directory.Exists(LOGS))
-        {
-            string[] files = Directory.GetFiles(LOGS);
-            DateTime cutoffDate = DateTime.Now.AddDays(-Config.CounterstrikeSharpMoreThanXdaysOld);
-
-            foreach (string file in files)
+            string folderPath = Path.Combine(ModuleDirectory, "../../plugins");
+            for (int i = 0; i < 1; i++)
             {
-                DateTime lastWriteTime = File.GetLastWriteTime(file);
+                folderPath = Path.Combine(folderPath, "..");
+            }
+            string LOGS = Path.Combine(folderPath, "logs");
 
-                if (lastWriteTime < cutoffDate)
+            if (Directory.Exists(LOGS))
+            {
+                string[] files = Directory.GetFiles(LOGS);
+                DateTime cutoffDate = DateTime.Now.AddDays(-Config.CounterstrikeSharpMoreThanXdaysOld);
+
+                foreach (string file in files)
                 {
-                    File.Delete(file);
+                    try
+                    {
+                        DateTime lastWriteTime = File.GetLastWriteTime(file);
+
+                        if (lastWriteTime < cutoffDate)
+                        {
+                            File.Delete(file);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error deleting file {file}: {ex.Message}");
+                    }
                 }
             }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error accessing directory: {ex.Message}");
         }
     }
     private void DeleteBackUP()
     {
-        string folderPath = Path.Combine(ModuleDirectory, "../../plugins");
-        for (int i = 0; i < 3; i++)
+        try
         {
-            folderPath = Path.Combine(folderPath, "..");
-        }
-
-        string[] files = Directory.GetFiles(folderPath, "backup_round*.txt");
-
-        DateTime cutoffDate = DateTime.Now.AddDays(-Config.BackupRoundMoreThanXdaysOld);
-
-        foreach (string file in files)
-        {
-            DateTime lastWriteTime = File.GetLastWriteTime(file);
-
-            if (lastWriteTime < cutoffDate)
+            string folderPath = Path.Combine(ModuleDirectory, "../../plugins");
+            for (int i = 0; i < 3; i++)
             {
-                File.Delete(file);
+                folderPath = Path.Combine(folderPath, "..");
             }
+
+            string[] files = Directory.GetFiles(folderPath, "backup_round*.txt");
+
+            DateTime cutoffDate = DateTime.Now.AddDays(-Config.BackupRoundMoreThanXdaysOld);
+
+            foreach (string file in files)
+            {
+                try
+                {
+                    DateTime lastWriteTime = File.GetLastWriteTime(file);
+
+                    if (lastWriteTime < cutoffDate)
+                    {
+                        File.Delete(file);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error deleting file {file}: {ex.Message}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error accessing directory: {ex.Message}");
+        }
+    }
+    private void DeleteDemos()
+    {
+        try
+        {
+            string folderPath = Path.Combine(ModuleDirectory, "../../plugins");
+
+            for (int i = 0; i < 3; i++)
+            {
+                folderPath = Path.Combine(folderPath, "..");
+            }
+
+            string[] files = Directory.GetFiles(folderPath, "*.dem");
+
+            DateTime cutoffDate = DateTime.Now.AddDays(-Config.DemoMoreThanXdaysOld);
+
+            foreach (string file in files)
+            {
+                try
+                {
+                    DateTime lastWriteTime = File.GetLastWriteTime(file);
+
+                    if (lastWriteTime < cutoffDate)
+                    {
+                        File.Delete(file);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error deleting file {file}: {ex.Message}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error accessing directory: {ex.Message}");
         }
     }
 }
