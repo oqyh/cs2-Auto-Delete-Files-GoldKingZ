f8be2b51b5f72ab3e7ae20a0bbb4a944faac1c5a
Gold KingZ <48490385+oqyh@users.noreply.github.com>
2024-01-17 04:47:33 +0400

    1.0.0

---
+++ Auto_Delete_Logs.cs
@@ -0,0 +1,84 @@
+using System.Text.Json.Serialization;
+using CounterStrikeSharp.API.Core;
+
+namespace Auto_Delete_Logs;
+
+public class AutoDeleteLogsConfig : BasePluginConfig
+{
+    [JsonPropertyName("CounterstrikeSharpMoreThanXdaysOld")] public int CounterstrikeSharpMoreThanXdaysOld { get; set; } = 0;
+    [JsonPropertyName("BackupRoundMoreThanXdaysOld")] public int BackupRoundMoreThanXdaysOld { get; set; } = 0;
+}
+
+public class AutoDeleteLogs : BasePlugin, IPluginConfig<AutoDeleteLogsConfig>
+{
+    public override string ModuleName => "Auto Delete Logs";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "Gold KingZ";
+    public override string ModuleDescription => "Auto Delete Logs Files";
+    public AutoDeleteLogsConfig Config { get; set; } = new AutoDeleteLogsConfig();
+    public bool deletefolder = false;
+    public void OnConfigParsed(AutoDeleteLogsConfig config)
+    {
+        Config = config;
+    }
+    
+    public override void Load(bool hotReload)
+    {
+        if(Config.CounterstrikeSharpMoreThanXdaysOld > 0)
+        {
+            DeleteCounterstrikeSharp();
+        }
+
+        if(Config.BackupRoundMoreThanXdaysOld > 0)
+        {
+            DeleteBackUP();
+        }
+    }
+    private void DeleteCounterstrikeSharp()
+    {
+        string folderPath = Path.Combine(ModuleDirectory, "../../plugins");
+        for (int i = 0; i < 1; i++)
+        {
+            folderPath = Path.Combine(folderPath, "..");
+        }
+        string LOGS = Path.Combine(folderPath, "logs");
+
+        if (Directory.Exists(LOGS))
+        {
+            string[] files = Directory.GetFiles(LOGS);
+            DateTime cutoffDate = DateTime.Now.AddDays(-Config.CounterstrikeSharpMoreThanXdaysOld);
+
+            foreach (string file in files)
+            {
+                DateTime lastWriteTime = File.GetLastWriteTime(file);
+
+                if (lastWriteTime < cutoffDate)
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+    }
+    private void DeleteBackUP()
+    {
+        string folderPath = Path.Combine(ModuleDirectory, "../../plugins");
+        for (int i = 0; i < 3; i++)
+        {
+            folderPath = Path.Combine(folderPath, "..");
+        }
+
+        string[] files = Directory.GetFiles(folderPath, "backup_round*.txt");
+
+        DateTime cutoffDate = DateTime.Now.AddDays(-Config.BackupRoundMoreThanXdaysOld);
+
+        foreach (string file in files)
+        {
+            DateTime lastWriteTime = File.GetLastWriteTime(file);
+
+            if (lastWriteTime < cutoffDate)
+            {
+                File.Delete(file);
+            }
+        }
+    }
+}
